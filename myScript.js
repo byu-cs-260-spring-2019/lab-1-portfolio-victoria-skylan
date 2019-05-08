a15bb6e6cf9e0cd3b27c2ccb9686b5e0c73cfaf6
victoria-skylan <the.vic.black@gmail.com>
2019-05-07 22:17:54 -0600

    first commit

---
+++ myScript.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+window.onload = function() {
+	document.getElementById('button').addEventListener('click', function() {
+		document.getElementById("details").style.display = "block";
+	});
+}
